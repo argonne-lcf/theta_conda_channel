a8752b8b5e0011e45fa20745a454b207bd766a94
Kyle Gerard Felker <kfelker@princeton.edu>
2020-01-21 22:17:33 +0000

    Require 1x argument for target miniconda install directory

--- conda_build_theta.sh
+++ conda_build_theta.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 INSTALL_DIRECTORY" >&2
+  exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget -N https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
 
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $1
 
 export PATH=$1/bin:$PATH
+conda_exe=$1/bin/conda
 
-conda install -y tensorflow=1.14 pytorch-cpu=1.2 mkl-dnn scipy pandas h5py virtualenv matplotlib scikit-learn 
-
-conda install -y -c alcf-theta mpi4py horovod
+${conda_exe} install -y tensorflow=1.14 pytorch-cpu=1.2 mkl-dnn scipy pandas h5py virtualenv matplotlib scikit-learn
+${conda_exe} install -y -c alcf-theta mpi4py horovod
